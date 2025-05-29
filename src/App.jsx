bde612180324dddb91c6831b7801bdc5c42e44f0
Umberto Amenta <umberto.amenta@hotmail.it>
2025-05-29 19:48:47 +0200

    feat: milestone 2 (implementare la ricerca ottimizzata)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+
+import SearchBar from "./components/SearchBar";
 
 export default function App() {
   const [politicians, setPoliticians] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const getPoliticians = async () => {
@@ -18,12 +21,20 @@ export default function App() {
     getPoliticians();
   }, []);
 
-  console.log(politicians);
+  const filteredPoliticians = useMemo(() => {
+    const lowerSearch = search.toLowerCase();
+    return politicians.filter(
+      (p) =>
+        p.name.toLowerCase().includes(lowerSearch) ||
+        p.biography.toLowerCase().includes(lowerSearch)
+    );
+  }, [politicians, search]);
 
   return (
     <>
+      <SearchBar search={search} setSearch={setSearch} />
       <div className="grid">
-        {politicians.map((p, i) => (
+        {filteredPoliticians.map((p, i) => (
           <div key={i} className="card">
             <img src={p.image} alt={p.name} />
             <div className="info">
